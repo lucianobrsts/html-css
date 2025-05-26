72c4ac4029f88588b497c25af8b4c5d11877342f
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-05-26 00:22:09 -0300

    Finalizado arquivo js e projeto

--- dark_mode/js/scripts.js
+++ dark_mode/js/scripts.js
@@ -1,5 +1,28 @@
 const changeThemeBtn = document.querySelector("#change-theme");
 
-changeThemeBtn.addEventListener("change", function() {
+//Toggle dark mode
+function toggleDarkMode() {
     document.body.classList.toggle("dark");
-})
+}
+
+// load light or dark mode
+function loadTheme() {
+    const darkMode = localStorage.getItem("dark");
+
+    if(darkMode) {
+        toggleDarkMode();
+    }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change", function() {
+    toggleDarkMode();
+
+    // save or remove dark mode
+    localStorage.removeItem("dark");
+
+    if(document.body.classList.contains("dark")) {
+        localStorage.setItem("dark", 1)
+    }
+});
