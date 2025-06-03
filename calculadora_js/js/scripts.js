6ad7252c7932bb890981d4aba4b56032e44df9be
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-06-02 22:27:07 -0300

    Adicionado valores a tela da calculadora

--- calculadora_js/js/scripts.js
+++ calculadora_js/js/scripts.js
@@ -3,13 +3,36 @@ const currentOperationText = document.querySelector("#current-operation")
 const buttons = document.querySelectorAll("#buttons-container button")
 
 class Calculator {
+    constructor(previousOperationText, currentOperationText) {
+        this.previousOperationText = previousOperationText;
+        this.currentOperationText = currentOperationText;
+        this.currentOperation = "";
+    }
 
+    // add digit to calculator screen
+    addDigit(digit) {
+        this.currentOperation = digit
+        this.updatescreen()
+    }
+
+    // change values of the calculator screen
+    updatescreen() {
+        this.currentOperationText.innerText += this.currentOperation;
+    }
 }
+
+
+
+const calc = new Calculator(previousOperationText, currentOperationText);
 
 buttons.forEach((btn) => {
     btn.addEventListener("click", (e) => {
         const value = e.target.innerText;
 
-        console.log(value);
+       if(+value >= 0 || value === ".") {
+        calc.addDigit(value);
+       } else {
+        console.log("Op: " + value);
+       }
     })
 })
